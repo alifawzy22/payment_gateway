09ec40389d3bc7cc95712a8a3b83b7b652305097
X4L9^mJ#t4Hs7MS <ali.fawzy.ali1997@gmail.com>
2025-02-10 12:14:33 +0200

    done ui

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:payment_gateway/core/utils/assets_data.dart';
 import 'package:payment_gateway/core/utils/styles.dart';
 import 'package:payment_gateway/core/utils/widgets/custom_elevated_button.dart';
 import 'package:payment_gateway/core/utils/widgets/custom_row_info.dart';
-import 'package:payment_gateway/features/home/presentation/views/payment_details_view.dart';
+import 'package:payment_gateway/features/home/presentation/views/thank_you_view.dart';
+import 'package:payment_gateway/features/home/presentation/views/widgets/custom_paid_button.dart';
+import 'package:payment_gateway/features/home/presentation/views/widgets/payment_methods_list_view.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({
@@ -64,11 +67,12 @@ class HomeViewBody extends StatelessWidget {
             label: 'Complete Payment',
             labelTextStyle: Styles.style22,
             onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => PaymentDetailsView(),
-                ),
-              );
+              // Navigator.of(context).push(
+              //   MaterialPageRoute(
+              //     builder: (context) => PaymentDetailsView(),
+              //   ),
+              // );
+              showModalSheet(context);
             },
           ),
         ),
@@ -76,6 +80,39 @@ class HomeViewBody extends StatelessWidget {
           height: 16,
         ),
       ],
+    );
+  }
+
+  showModalSheet(BuildContext context) {
+    return showMaterialModalBottomSheet(
+      context: context,
+      builder: (context) => SingleChildScrollView(
+        controller: ModalScrollController.of(context),
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 32),
+          decoration: ShapeDecoration(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              PaymentMethodsListView(),
+              SizedBox(
+                height: 16,
+              ),
+              CustomPAIDButton(
+                text: 'PAID',
+                onPressed: () {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => ThankYouView()));
+                },
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
